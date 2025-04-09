f9d4f20ab77d943ebb9ab0c5b9c4ef1a85785d3e
tonfi filho <oficinadosdrones@gmail.com>
2025-04-09 15:37:00 +0100

    2º commit

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,7 +2,29 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
+import { getAuth } from 'firebase/auth';
+import { environment } from '../environment/environment';
+
+
+// Inicializa o Firebase
+ const firebaseApp = initializeApp(environment.firebase)
+export const firestore = getFirestore(firebaseApp);
+export const auth = getAuth(firebaseApp);
+
+
+
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes),
+    // Fornece as instâncias do Firebase para injeção de dependência
+    { provide: 'FIREBASE_APP', useValue: firebaseApp },
+    { provide: 'FIREBASE_FIRESTORE', useValue: firestore },
+    { provide: 'FIREBASE_AUTH', useValue: auth },
+
+
+
+
+  ]
 };
---
+++ src/environment/environment.ts
@@ -0,0 +1,15 @@
+export const environment = {
+    firebase: {
+      apiKey: "AIzaSyCgFRFPipVWIDumqKMuNZGlX0_SMn4ohTg",
+      authDomain: "mydata4u-tony-filho.firebaseapp.com",
+      projectId: "mydata4u-tony-filho",
+      storageBucket: "mydata4u-tony-filho.firebasestorage.app",
+      messagingSenderId: "900684711835",
+      appId: "1:900684711835:web:c31b58b412a4fe4898433e",
+      measurementId: "G-N7WWWTGRZL"
+    },
+  
+    production:false
+  
+  
+  };

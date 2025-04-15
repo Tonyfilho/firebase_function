b4c63b6fa0a9d4cb9f1c1e317c283076953cddb0
tonfi filho <oficinadosdrones@gmail.com>
2025-04-15 15:31:48 +0100

    update index fucntions

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,56 +1,43 @@
-/**
- * Import function triggers from their respective submodules:
- *
- * import {onCall} from "firebase-functions/v2/https";
- * import {onDocumentWritten} from "firebase-functions/v2/firestore";
- *
- * See a full list of supported triggers at https://firebase.google.com/docs/functions
- */
-
-// import {onRequest} from "firebase-functions/v2/https";
-// import * as logger from "firebase-functions/logger";
-
-// Start writing functions
-// https://firebase.google.com/docs/functions/typescript
-
-// export const helloWorld = onRequest((request, response) => {
-//   logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
-
-const functions = require("firebase-functions");
-const admin = require("firebase-admin");
+const functions = require('firebase-functions');
+const admin = require('firebase-admin');
 admin.initializeApp();
 //const db = admin.firestore();
-const cors = require("cors")({origin: true});
-import {Request, Response} from "express";
-
-
-
+const cors = require('cors')({ origin: true });
+import { Request, Response } from 'express';
 
 interface RandomNumberRequest extends Request {}
 interface RandomNumberResponse extends Response {}
 
 // http request  pode ser chamado pelo postman ou pelo front-end, ou direto no navegador, copiando o link da url do firebase lá console firebase
 // http request 1, neste caso será um redirest
-exports.randomNumber = functions.https.onRequest((req: RandomNumberRequest, res: RandomNumberResponse) => {
+exports.randomNumber = functions.https.onRequest(
+  (req: RandomNumberRequest, res: RandomNumberResponse) => {
     cors(req, res, () => {
-        const randomNumber: number = Math.round(Math.random() * 100) + 1;
-        // res.json({randomNumber});
-        res.send(`Random number: ${randomNumber}`);
+      const randomNumber: number = Math.round(Math.random() * 100) + 1;
+      // res.json({randomNumber});
+      res.send(`Random number: ${randomNumber}`);
     });
-});
+  }
+);
 // http request 2, neste caso será um redirest
-exports.toTheDojo = functions.https.onRequest((req: RandomNumberRequest, res: RandomNumberResponse) => {
+exports.toTheDojo = functions.https.onRequest(
+  (req: RandomNumberRequest, res: RandomNumberResponse) => {
     cors(req, res, () => {
-         res.redirect("https://tonyfilhocurriculum.web.app/");
-       
+      res.redirect('https://tonyfilhocurriculum.web.app/');
     });
-});
+  }
+);
 
+/* OBS: Callable function precisa se chamanda no front-end com o firebase callable function.
+Callable não se retorna o Data, ao contrario do Request Functions onde retornamos o RESPONSE
+Neste caso, o retorno é feito pelo firebase, e não pelo código.
+Callable function não precisa de cors, pois o firebase já faz isso para nós.
+*/
 
-// callable function precisa se chamanda no front-end com o firebase callable function.
-
+/**
+ * Data serve para passar dados para a função, no caso o nome, ou seja vem do Front-end.
+ * Context serve para passar o contexto da função, no caso o usuário que está chamando a função
+ */
 exports.sayHello = functions.https.onCall((data: any, context: any) => {
-   return  "Hello Tony!";
+  return `Hello ${data.name} `;
 });
